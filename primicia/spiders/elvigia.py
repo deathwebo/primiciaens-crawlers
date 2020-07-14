54e49b75a680a25e9e00f1a72c14825ac32810e2
Erick Márquez <erick.marquez@britecore.com>
2020-07-13 20:50:05 -0700

    Filter stories from elvigia

--- primicia/spiders/elvigia.py
+++ primicia/spiders/elvigia.py
@@ -9,6 +9,9 @@ locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 now = datetime.now()
 today_formatted = now.strftime('%A, %-d de %B de %Y')
 
+CATEGORIES = ["GENERAL", "EL VALLE", "911", "COLUMNAS"]
+
+
 class ElVigiaSpider(scrapy.Spider):
     name = 'elvigia'
     start_urls = ['https://elvigia.net/archivo/']
@@ -18,11 +21,15 @@ class ElVigiaSpider(scrapy.Spider):
         if subtitulo != today_formatted:
             raise CloseSpider('finished_today')
 
-        for title in response.css('.listado-noticias>li'):
-            relative_url = title.css('a ::attr(href)').get()
-            url = urljoin(response.request.url, relative_url)
-            yield {
-                'title': title.css('a ::text').get(),
-                'url': url,
-                'website': 'elvigia.net'
-            }
+        for archivo in response.css('.archivo'):
+            category = archivo.css(".todoslostitulos-titulo::text").get()
+            if category not in CATEGORIES:
+                continue
+            for title in archivo.css('.listado-noticias>li'):
+                relative_url = title.css('a ::attr(href)').get()
+                url = urljoin(response.request.url, relative_url)
+                yield {
+                    'title': title.css('a ::text').get(),
+                    'url': url,
+                    'website': 'elvigia.net'
+                }
